525fcbba42544c5afc058f07709986d3ff653d60
Andreas Fruth <andreas.fruth@gmail.com>
2016-11-02 11:43:44 +0200

    Finished adding vue. Added small example

---
+++ src/client/app/components/TestRoute/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="vuediv">Hello {{ msg }}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "VUE"
+            }
+        }
+    }
+</script>
+
+<style>
+    .vuediv {
+        color: red
+    }
+</style>
--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-alert('It works')
+import TestRoute from 'app/components/TestRoute/index.vue';
+
+Vue.use(VueRouter);
+
+const Bar = { template: '<div>This is the bar route</div>' };
+
+const routes = [
+  { path: '/test', component: TestRoute },
+  { path: '/bar', component: Bar },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+/* eslint-disable no-new, no-unused-vars */
+const app = new Vue({
+  router,
+  el: '#app',
+});
