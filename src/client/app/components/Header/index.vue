4880e878d4a5c69a168c8f34778dc83dc32a986b
Andreas Fruth <andreas.fruth@gmail.com>
2016-11-03 16:25:58 +0200

    Individual links

--- src/client/app/components/Header/index.vue
+++ src/client/app/components/Header/index.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
         <router-link to="/">Go to Home</router-link>
+        <router-link to="/projects">Go to Projects</router-link>
+        <router-link to="/projects/123123">Go to individual project</router-link>
+        <router-link to="/members">Go to Members</router-link>
+        <router-link to="/members/131212">Go to individual member</router-link>
+        <router-link to="/communities">Go to Communities</router-link>
+        <router-link to="/communities/123123">Go to individual community</router-link>
     </div>
 </template>
